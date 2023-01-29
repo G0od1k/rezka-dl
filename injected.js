2988ad388c7767faf99155f46622177c6a678dc0
G0od1k <g0cd1k@gmail.com>
2023-01-29 03:03:25 +0200

    Initial commit

---
+++ injected.js
@@ -0,0 +1,45 @@
+const rdl = {}
+
+;(async () => {
+    const vttRegExp = /vtt$/
+    const videoRegExp = /.+stream\.voidboost\.cc\/.*\.mp4.*/
+
+    interval = setInterval(() => {
+        let network = performance
+            .getEntriesByType("resource")
+            .map((x) => x.name)
+            .filter((x) => vttRegExp.test(x) || videoRegExp.test(x))
+
+        network.forEach((url) => {
+            let id =
+                [".b-simple_seasons__list", ".b-simple_episodes__list"]
+                    .map(
+                        (x) =>
+                            document
+                                .querySelector(x + ">li.active")
+                                ?.textContent?.match(/\d+/)[0] ?? "0"
+                    )
+                    .join("s") + "e"
+
+            rdl[id] ??= {}
+
+            if (vttRegExp.test(url)) {
+                rdl[id].vtt = url
+            } else {
+                rdl[id].url = url.replace(/(.+\.mp4).+/, "$1")
+            }
+        })
+
+        rdl.name = document.querySelector(
+            `#main div.b-post__origtitle`
+        )?.textContent
+
+        performance.clearResourceTimings()
+    }, 500)
+})()
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message == "req-rdl") {
+        chrome.runtime.sendMessage(JSON.stringify(rdl))
+    }
+})
---
+++ script.js
@@ -0,0 +1,102 @@
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, "req-rdl")
+})
+
+chrome.runtime.onMessage.addListener((message) => {
+    let json = JSON.parse(message)
+
+    document.querySelector(`input#name`).placeholder = json.name
+
+    let seasons = getSeasons(json).reverse()
+
+    seasons.forEach((x) => {
+        let h = document.createElement(`h2`),
+            epList = document.createElement(`div`)
+
+        h.textContent = x
+        h.id = `h${x}s`
+        epList.className = `ep_list`
+        epList.id = `ep_l${x}s`
+
+        document.querySelector(`#name`).after(epList)
+        document.querySelector(`#name`).after(h)
+    })
+
+    Object.keys(json)
+        .filter((x) => x != "name")
+        .sort((a, b) => a.match(/\d+/g)[1] - b.match(/\d+/g)[1])
+        .forEach((x) => {
+            let episodeNode = document
+                .querySelector("template#ep")
+                .content.firstElementChild.cloneNode(true)
+
+            episodeNode.id = `e` + x
+
+            episodeNode.querySelector(".name").textContent = x
+
+            if (!json[x].vtt) {
+                episodeNode.querySelector(".vtt").disabled = true
+                episodeNode.querySelector(".vtt").checked = false
+            }
+
+            document
+                .querySelector(`#ep_l${x.match(/\d+/)[0]}s`)
+                .appendChild(episodeNode)
+        })
+
+    document.querySelector(`#download`).onclick = function () {
+        if (isFilm(json)) {
+            download(json["0s0e"].url, json.name + ".mp4")
+            vttQ("0s0e")
+                ? download(json["0s0e"].vtt, json.name + ".vtt")
+                : false
+            return
+        }
+        getSortedEpisodes(json).forEach((x) => {
+            if (epQ(x)) {
+                download(json[x].url, `${json.name}_${x}.mp4`)
+                vttQ(x) ? download(json[x].vtt, `${json.name}_${x}.vtt`) : false
+            }
+        })
+    }
+})
+
+function getSeasons(json) {
+    return Object.keys(json)
+        .filter((x) => x != "name")
+        .map((x) => getSe(x))
+        .filter((v, i, a) => a.indexOf(v) === i)
+        .sort((a, b) => a - b)
+}
+
+function isFilm(json) {
+    return Object.keys(json).join("") == "name0s0e"
+}
+
+function vttQ(ep) {
+    return document.querySelector(`#e${ep}>.vtt`).checked
+}
+
+function epQ(ep) {
+    return document.querySelector(`#e${ep}>.epq`).checked
+}
+
+function getSortedEpisodes(json) {
+    return Object.keys(json)
+        .filter((x) => x != "name")
+        .sort((a, b) => {
+            return getEp(a) - getEp(b) && getSe(a) - getSe(b)
+        })
+}
+
+function getSe(string) {
+    return string.match(/\d+/g)[0]
+}
+
+function getEp(string) {
+    return string.match(/\d+/g)[1]
+}
+
+function download(url, name) {
+    chrome.downloads.download({ url: url, filename: name })
+}
